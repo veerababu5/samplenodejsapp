bf1cdb1cb5200fb1a0e24767cc42ee7e26ce7740
root <root@ip-172-31-0-219.us-west-2.compute.internal>
2022-11-27 15:52:16 +0000

    files added

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:gray;">Java Home App - CI/CD Jenkins1234!!</h1> \n');
+  res.send('<h1 style="color:blue;">Welcome to Supplier Quality Assessment version: 01</h1> \n');
+  res.send('<h2 style="color:red;">Version:01</h2> \n');
 });
 
 app.listen(PORT, HOST);
